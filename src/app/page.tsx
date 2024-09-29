8409b92f7b4f2fe8b0540ea7216ca6e5e77f5f28
soezyxstt <16922204@mahasiswa.itb.ac.id>
2024-09-29 22:20:18 +0700

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
           2024
         </Title>
       </div>
-      <Image src={'/v-1.png'} alt={'v-1'} width={1000} height={1000} className='md:h-[45%] min-w-[150vw] relative left-1/2 -translate-x-1/2 md:min-w-auto'/>
+      <Image src={'/v-1.png'} alt={'v-1'} width={1000} height={1000} className='md:h-[45%] md:min-w-[auto] min-w-[150vw] relative left-1/2 -translate-x-1/2 md:min-w-auto'/>
       <Image src={'/v-14.png'} alt={'v-1'} width={1000} height={1000} className='min-w-[140%] md:min-w-full h-[33%] md:h-auto bottom-0 absolute'/>
       <Image src={'/v-16.png'} alt={'v-1'} width={1000} height={1000}
              className='md:min-w-full min-w-[200vw] bottom-0 translate-y-1/2 absolute'/>
