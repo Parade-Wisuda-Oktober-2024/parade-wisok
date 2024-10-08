446bf4bd652ca22e1ec4c7886579aacf2addd9ed
wisaahmaduta <wiseowisa@gmail.com>
2024-10-08 20:53:49 +0700

    add navbar and footer fragments
    note: reminder to change each Link href with the designated route

---
+++ src/app/navbar.tsx
@@ -0,0 +1,78 @@
+'use client'
+import Image from "next/image"
+import logo from "../../public/logo.png"
+import burger from "../../public/burger.svg"
+import { usePathname } from "next/navigation"
+import { useEffect } from "react"
+import Link from "next/link"
+import { useState } from "react"
+import Sidebar from "./sidebar"
+
+const NavBar = () => {
+  const pathname = usePathname()
+  const [taFair, setTaFair] = useState(false)
+  const [graduates, setGraduates] = useState(false)
+  const [merchandise, setMerchandise] = useState(false)
+  const [sponshorship, setSponsorship] = useState(false)
+  const [aboutUs, setAboutUs] = useState(false)
+  const [sidebar, setSidebar] = useState(false)
+  useEffect(()=> {
+    setTaFair(false)
+    setGraduates(false)
+    setMerchandise(false)
+    setSponsorship(false)
+    setAboutUs(false)
+    if (pathname == "/TaFair") setTaFair(true)
+    else if (pathname == "/Graduates") setGraduates(true)
+    else if (pathname == "/Merchandise") setMerchandise(true)
+    else if (pathname == "/Sponsorship") setSponsorship(true)
+    else if (pathname == "/AboutUs") setAboutUs(true)
+  },[pathname])
+  return(
+    <div>
+      {sidebar && <Sidebar setSidebar={setSidebar} taFair={taFair} graduates={graduates} merchandise={merchandise} sponshorship={sponshorship} aboutUs={aboutUs} />}
+      <div className="h-[73px] xl:h-[100px] w-full">
+        <div className="bg-[url('/navbar-bg.png')] w-full h-full bg-center relative bg-cover">
+          <div className="bg-[url('/ornament.png')] w-full h-full bg-center bg-cover flex xl:justify-between px-4 xl:px-[40px] bg-opacity-60">
+            <div className="flex items-center justify-between gap-2 w-full">
+              <Link href="/">
+                <div className="w-full flex items-center gap-2">
+                  <div className="w-[68.124px] h-[61px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]"><Image src={logo} alt="logo wisokto" objectFit="contain"/></div>
+                  <div className="h-[28px] font-bold text-3xl text-[#F9BE6B] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] whitespace-nowrap ">WISOKTO 2024</div>
+                </div>
+              </Link>
+              <div onClick={()=>setSidebar(true)} className="w-[23.9px] h-[18.59px] xl:hidden"><Image src={burger} alt="dropdown" objectFit="contain"/></div>
+            </div>
+            <div className="hidden xl:flex gap-[12px] text-[#F9BE6B] font-bold text-[28px] items-center whitespace-nowrap">
+              <Link href="/TaFair">
+                <div className={`p-[10px] ${!taFair ? 'hover:bg-[#FF8CD926]': ''} rounded-lg transition-all`}>TA Fair<div className={`h-1 bg-[#F9BE6B] rounded-lg transform origin-top transition-all ease-out duration-300 ${taFair ? '-scale-y-100 h-1 opacity-100' : 'scale-y-0 h-0 opacity-0'}`}></div></div>
+              </Link>
+              <Link href="/Graduates">
+                <div className={`p-[10px] ${!graduates ? 'hover:bg-[#FF8CD926]': ''} rounded-lg transition-all`}>Graduates<div className={`h-1 bg-[#F9BE6B] rounded-lg transform origin-top transition-all ease-out duration-300 ${graduates ? '-scale-y-100 h-1 opacity-100' : 'scale-y-0 h-0 opacity-0'}`}></div></div>
+              </Link>
+              <Link href="/Merchandise">
+                <div className={`p-[10px] ${!merchandise ? 'hover:bg-[#FF8CD926]': ''} rounded-lg transition-all`}>Merchandise<div className={`h-1 bg-[#F9BE6B] rounded-lg transform origin-top transition-all ease-out duration-300 ${merchandise ? '-scale-y-100 h-1 opacity-100' : 'scale-y-0 h-0 opacity-0'}`}></div></div>
+              </Link>
+              <Link href="/Sponsorship">
+                <div className={`p-[10px] ${!sponshorship ? 'hover:bg-[#FF8CD926]': ''} rounded-lg transition-all`}>Sponsorship<div className={`h-1 bg-[#F9BE6B] rounded-lg transform origin-top transition-all ease-out duration-300 ${sponshorship ? '-scale-y-100 h-1 opacity-100' : 'scale-y-0 h-0 opacity-0'}`}></div></div>
+              </Link>
+              <Link href="/AboutUs">
+                <div className={`p-[10px] ${!aboutUs ? 'hover:bg-[#FF8CD926]': ''} rounded-lg transition-all`}>About us<div className={`h-1 bg-[#F9BE6B] rounded-lg transform origin-top transition-all ease-out duration-300 ${aboutUs ? '-scale-y-100 h-1 opacity-100' : 'scale-y-0 h-0 opacity-0'}`}></div></div>
+              </Link>
+              <div>
+                <Link href="/Login">
+                  <div className="py-[12px] px-[32px] bg-[#210B3A] rounded-lg border-none">
+                    Login
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Title from "~/components/title";
 import Image from "next/image";
+import NavBar from "./navbar";
+import Footer from "./footer";
 
 export default function HomePage() {
 
   return (
     <main
       className={`flex flex-col relative justify-end min-h-dvh max-h-dvh overflow-hidden font-header bg-gradient-to-bl from-[#320166] to-[#42076a]`}>
+      <NavBar/>
       <div className="flex flex-col items-center flex-1 h-full justify-center">
         <Title>
           PARADE
@@ -19,8 +22,10 @@ export default function HomePage() {
       </div>
       <Image src={'/v-1.png'} alt={'v-1'} width={1000} height={1000} className='md:h-[45%] md:min-w-[auto] min-w-[150vw] relative left-1/2 -translate-x-1/2 md:min-w-auto'/>
       <Image src={'/v-14.png'} alt={'v-1'} width={1000} height={1000} className='min-w-[140%] md:min-w-full h-[33%] md:h-auto bottom-0 absolute'/>
-      <Image src={'/v-16.png'} alt={'v-1'} width={1000} height={1000}
-             className='md:min-w-full min-w-[200vw] bottom-0 translate-y-1/2 absolute'/>
+      <Image src={'/v-16.png'} alt={'v-1'} width={1000} height={1000} className='md:min-w-full min-w-[200vw] bottom-0 translate-y-1/2 absolute'/>
+      <div className="fixed z-10 w-full">
+        <Footer/>
+      </div>
     </main>
   );
 }
