1eec5d4a1ab5bfa939bbc8f13f63c85288695567
Nadhif <nadhifalrozin@gmail.com>
2024-10-10 06:26:41 +0700

    menambah padding, merubah font, ukuran dan warna item

--- src/components/FAQ/FAQitem.tsx
+++ src/components/FAQ/FAQitem.tsx
@@ -16,12 +16,11 @@ const FAQItem: React.FC<FAQItemProps> = ({ question, answer, value }) => {
     return (
         <div className="md:w-[70%] w-[80%] flex flex-col items-center justify-center rounded-xl border-[3px] border-[#9803f591] bg-gradient-to-b from-[#10092b4a] to-[#03010823] mb-4">            
                 <AccordionItem className="w-full" value={value}>
-                    <AccordionTrigger>{question}</AccordionTrigger>
-                        <AccordionContent>
+                    <AccordionTrigger className="px-5 text-2xl font-[Fraunces] text-[#D9D9D9] font-bold">{question}</AccordionTrigger>
+                        <AccordionContent className="px-5 font-[Fraunces] text-[#D9D9D9]">
                         {answer}
                         </AccordionContent>
                 </AccordionItem>
-
         </div>
     );
 };
