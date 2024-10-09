42f9fb3a04295199a56fbb69c939ddb2e19ba327
andi-frame <andifarhan1094@gmail.com>
2024-10-09 23:44:34 +0700

    feat: add testing component as example for easier integration

---
+++ src/components/testing/SearchTAExp.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { useState } from "react";
+import { useAction } from "next-safe-action/hooks";
+import { searchTA } from "~/app/actions/search-TA";
+
+const SearchTAExp = () => {
+  const { execute, result } = useAction(searchTA);
+  const [page, setPage] = useState(1);
+  const [name, setName] = useState<string>("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    setName(input);
+
+    execute({ wisudawanName: input, faculty: "", major: "", page: page });
+  };
+
+  const buttonOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const value = Number(e.currentTarget.value);
+    setPage(value);
+
+    execute({ wisudawanName: name, faculty: "", major: "", page: value });
+  };
+
+  return (
+    <div className="m-5">
+      <div className="flex flex-col gap-5">
+        {result.data && result.data.length > 0 ? (
+          result.data.map((ta) => (
+            <div key={ta.title}>
+              <h3>{ta.title}</h3>
+              <p>Wisudawan Name: {ta.wisudawanName}</p>
+              <p>Faculty: {ta.faculty}</p>
+              {/* <p>Major: {ta.major}</p> */}
+              <p>Likes: {ta.likeCount}</p>
+            </div>
+          ))
+        ) : (
+          <p>No TA found.</p>
+        )}
+      </div>
+
+      <div>
+        <div className="my-3">
+          <input
+            type="text"
+            value={name}
+            onChange={onChange}
+            placeholder="Search Wisudawan Name"
+          />
+        </div>
+        <div className="my-3 flex gap-5">
+          <button onClick={buttonOnClick} value={1}>
+            1
+          </button>
+          <button onClick={buttonOnClick} value={2}>
+            2
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchTAExp;
