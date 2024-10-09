10a8501bee351d9e45093920bea3b2ebb450bc82
Nadhif <nadhifalrozin@gmail.com>
2024-10-10 06:16:10 +0700

    merubah Collapsible menjadi accordion

--- src/components/FAQ/FAQitem.tsx
+++ src/components/FAQ/FAQitem.tsx
@@ -1,38 +1,27 @@
 "use client";
 
-import React, { useState } from "react";
 import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "../ui/collapsible";
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "../ui/accordion";
 
 interface FAQItemProps {
   question: string;
   answer: string;
+  value: string;
 }
 
-const FAQItem: React.FC<FAQItemProps> = ({ question, answer }) => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-
-    const toggleOpen = () => {
-        setIsOpen((prev) => !prev);
-    };
-
+const FAQItem: React.FC<FAQItemProps> = ({ question, answer, value }) => {
     return (
-        <div className="md:w-[70%] w-[80%] flex flex-col items-center justify-center rounded-xl border-[3px] border-[#9803f591] bg-gradient-to-b from-[#10092b4a] to-[#03010823] mb-4">
-            <Collapsible open={isOpen} onOpenChange={toggleOpen} className="w-full">
-                <CollapsibleTrigger className="w-full px-[3em] flex flex-row items-center justify-between cursor-pointer">
-                    <p className="text-2xl opacity-100">{question}</p>
-                    <img 
-                        src={isOpen ? '/keyboard_arrow_up.png' : '/keyboard_arrow_down.png'} 
-                        alt="Toggle" 
-                    />
-                </CollapsibleTrigger>
-                <CollapsibleContent className="opacity-100 w-full px-[3em] pb-[1em] flex flex-col items-start">
-                    <div>{answer}</div>
-                </CollapsibleContent>
-            </Collapsible>
+        <div className="md:w-[70%] w-[80%] flex flex-col items-center justify-center rounded-xl border-[3px] border-[#9803f591] bg-gradient-to-b from-[#10092b4a] to-[#03010823] mb-4">            
+                <AccordionItem className="w-full" value={value}>
+                    <AccordionTrigger>{question}</AccordionTrigger>
+                        <AccordionContent>
+                        {answer}
+                        </AccordionContent>
+                </AccordionItem>
+
         </div>
     );
 };
