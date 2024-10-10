6532aece8178642a1e0a6c73e32ddf658de0a66e
unknown <19623167@std.stei.itb.ac.id>
2024-10-10 21:25:36 +0700

    fix:fix mobile display

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,44 +1,43 @@
 "use client";
 import React, { useState } from 'react';
 import Image from 'next/image';
+import {signIn} from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+
+interface LoginInput {
+  username: string;
+  password: string;
+}
 
 
-const Login: React.FC = () => {
-    const [username, setUsername] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
-    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+export default function Login()  {
+  const router = useRouter();
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginInput>();
 
-    /*const handleLogin = async (e: React.FormEvent) => {
-      e.preventDefault();
-      setErrorMessage(null);
-      setSuccessMessage(null);
+  const onSubmit = async (data:LoginInput) => {
+    const result = await signIn('credentials', {
+      redirect: false,
+      username: data.username,
+      password: data.password,
+    });
 
-      try {
-        const response = await axios.post('http://localhost:3000/login', {
-          username,
-          password,
-        });
-
-        setSuccessMessage('Login berhasil!');
-        localStorage.setItem('token', response.data.token); // Simpan token JWT di local storage
-      } catch (error: any) {
-        if (error.response && error.response.data.message) {
-          setErrorMessage(error.response.data.message);
-        } else {
-          setErrorMessage('Terjadi kesalahan, coba lagi.');
-        }
-      }
-    };*/
+    if (result.error) {
+      console.error(result.error);
+      alert("Login failed: " + result.error);
+    } else {
+      router.push('/'); 
+    }
+  }
   return (
     <div
     className="h-screen flex items-center justify-center bg-opacity-80"
     style={{ background: 'linear-gradient(to right,  #92007F,#542980)' }}
   >
         <div className="max-w-2xl w-full h-[75%] max-h-[500px] px-20 pb-15 relative bg-[#3a0758] bg-opacity-35 rounded-lg shadow-lg text-center z-10">
-            <div className="relative flex flex-col items-center justify-center mb-10 mt-12">
+            <div className="relative flex flex-col items-center justify-center mb-10 mt-12 h-[100px]">
                 {/* Burung kiri */}
-                <Image src={'/login/VIS 11.png'} alt='burung' width={120} height={120} className='absolute bottom-[-60px] left-[170px] rotate-[140deg] sm:bottom-[-25px] sm:left-[130px] md:bottom-[-35px] md:left-[100px] lg:bottom-[-60px] lg:left-[75px] w-[30px] h-[30px] sm:w-[60px] sm:h-[60px] md:w-[80px] md:h-[80px] lg:w-[125px] lg:h-[125px]' />
+                <Image src={'/login/VIS 11.png'} alt='burung' width={120} height={120} className='absolute bottom-[15px] left-[15px] w-[60px] h-[30px] rotate-[140deg] sm:bottom-[-10px] sm:left-[120px] md:bottom-[-35px] md:left-[100px] lg:bottom-[-60px] lg:left-[75px] sm:w-[80px] sm:h-[60px] md:w-[80px] md:h-[80px] lg:w-[125px] lg:h-[125px]' />
                 
                 {/* Title */}
                 <h1 className="title bg-gradient-to-b from-text-alt to-text bg-clip-text text-transparent font-bold m-0 relative z-10 [text-shadow:2px_2px_10px_#FFF3D9] ">LOGIN
@@ -46,19 +45,19 @@ const Login: React.FC = () => {
                 
                 
                 {/* Burung kanan */}
-                <Image src={'/login/VIS 11.png'} alt='burung' width={120} height={120} className='absolute top-[-80px] right-[150px] rotate-[-50deg] sm:top-[-35px] sm:right-[115px] md:top-[-45px] md:right-[85px] lg:top-[-65px] lg:right-[50px] w-[40px] h-[40px] sm:w-[70px] sm:h-[70px] md:w-[110px] md:h-[110px] lg:w-[150px] lg:h-[150px] m-0'/>
+                <Image src={'/login/VIS 11.png'} alt='burung' width={120} height={120} className='absolute top-[15px] right-[15px]  w-[60px] h-[30px] rotate-[-50deg] sm:top-[-10px] sm:right-[115px] md:top-[-45px] md:right-[85px] lg:top-[-65px] lg:right-[50px] sm:w-[80px] sm:h-[60px] md:w-[110px] md:h-[110px] lg:w-[150px] lg:h-[150px] m-0'/>
             </div>
-            <form id="loginForm" className="space-y-1 py-2 relative h-[230px]">
+            <form id="loginForm" onSubmit={handleSubmit(onSubmit)} className="space-y-1 py-2 relative h-[230px]">
                 <p className='text-left text-white text-lg'>Username</p>
-                <input type="text" id="username" placeholder="Username" value={username} onChange={(e)=>setUsername(e.target.value)} className="w-full px-4 py-1  font-paragraph text-xl bg-[#8063bb] bg-opacity-60 text-white rounded-md focus:ring-2 focus:ring-pink-400 focus:outline-none"></input>
+                <input type="text" id="username" placeholder="Username" {...register('username', { required: 'username is required' })} className="w-full px-4 py-1  font-paragraph text-xl bg-[#8063bb] bg-opacity-60 text-white rounded-md focus:ring-2 focus:ring-pink-400 focus:outline-none"></input>
                 <p className='text-left text-white text-lg'>Password</p>
-                <input type="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-4 py-1 font-paragraph text-xl bg-[#8063bb] bg-opacity-60 text-white rounded-md focus:ring-2 focus:ring-pink-400 focus:outline-none"></input>
-                <button type="submit" className="absolute bottom-[10px] left-[350px] w-1/4  text-[#3a0758] bg-[#FFD866] bg-opacity-85 py-2 rounded-lg font-paragraph sm:left-[180px] sm:bottom-[15px] md:left-[190px] lg:left-[200px] lg:bottom-[20px] hover:bg-opacity-100 transition duration-300">
-                <Image src={'/login/VIS 11.png'} alt='burung' width={80} height={120} className='absolute bottom-[-60px] left-[170px] opacity-70 rotate-[135deg] sm:bottom-[-5px] sm:left-[-5px] md:bottom-[-8px] md:left-[-8px] lg:bottom-[-10px] lg:left-[-15px] w-[30px] h-[30px] sm:w-[70px] sm:h-[30px] md:w-[75px] md:h-[35px] lg:w-[100px] lg:h-[40px]' />
-                <Image src={'/login/VIS 12.png'} alt='bintang' width={120} height={120} className='absolute top-[-80px] right-[150px] opacity-70 sm:top-[22px] sm:right-[55px] md:top-[22px] md:right-[60px] lg:top-[21px] lg:right-[55px] w-[40px] h-[40px] sm:w-[15px] sm:h-[15px] md:w-[18px] md:h-[18px] lg:w-[20px] lg:h-[20px] m-0'/>
+                <input type="password" id="password" placeholder="Password"  {...register('password', { required: 'Password is required' })} className="w-full px-4 py-1 font-paragraph text-xl bg-[#8063bb] bg-opacity-60 text-white rounded-md focus:ring-2 focus:ring-pink-400 focus:outline-none"></input>
+                <button type="submit" className="absolute bottom-[10px] left-[80px] w-1/3  text-[#3a0758] bg-[#FFD866] bg-opacity-85 py-2 rounded-lg font-paragraph sm:left-[180px] sm:bottom-[15px] md:left-[190px] lg:left-[200px] lg:bottom-[20px] hover:bg-opacity-100 transition duration-300">
+                <Image src={'/login/VIS 11.png'} alt='burung' width={80} height={120} className='absolute bottom-[0px] left-[-10px] w-[40px] h-[20px] opacity-70 rotate-[135deg] sm:bottom-[-5px] sm:left-[15px] md:bottom-[-8px] md:left-[20px] lg:bottom-[-10px] lg:left-[5px] sm:w-[70px] sm:h-[30px] md:w-[75px] md:h-[35px] lg:w-[100px] lg:h-[40px]' />
+                <Image src={'/login/VIS 12.png'} alt='bintang' width={120} height={120} className='absolute bottom-[-2px] left-[20px]  w-[20px] h-[20px] opacity-70 sm:top-[22px] sm:left-[65px] md:top-[22px] md:left-[75px] lg:top-[21px] lg:left-[75px] sm:w-[15px] sm:h-[15px] md:w-[25px] md:h-[20px] lg:w-[30px] lg:h-[20px] m-0'/>
                 LOGIN
-                <Image src={'/login/VIS 12.png'} alt='bintang' width={120} height={120} className='absolute top-[-80px] right-[150px] opacity-70 sm:top-[0px] sm:right-[45px] md:top-[-2px] md:right-[45px] lg:top-[-2px] lg:right-[45px] w-[40px] h-[40px] sm:w-[15px] sm:h-[15px] md:w-[18px] md:h-[18px] lg:w-[20px] lg:h-[20px] m-0'/>
-                <Image src={'/login/VIS 11.png'} alt='burung' width={120} height={120} className='absolute top-[-80px] right-[150px] opacity-70 rotate-[-50deg] sm:top-[-6px] sm:right-[-5px] md:top-[-9px] md:right-[-10px] lg:top-[-12px] lg:right-[-20px] w-[40px] h-[40px] sm:w-[70px] sm:h-[30px] md:w-[75px] md:h-[35px] lg:w-[100px] lg:h-[40px] m-0'/>
+                <Image src={'/login/VIS 12.png'} alt='bintang' width={120} height={120} className='absolute top-[0px] right-[20px] w-[20px] h-[20px] opacity-70 sm:top-[0px] sm:right-[65px] md:top-[-2px] md:right-[65px] lg:top-[-2px] lg:right-[65px] sm:w-[15px] sm:h-[15px] md:w-[25px] md:h-[20px] lg:w-[30px] lg:h-[20px] m-0'/>
+                <Image src={'/login/VIS 11.png'} alt='burung' width={120} height={120} className='absolute top-[0px] right-[-10px] w-[40px] h-[20px] opacity-70 rotate-[-50deg] sm:top-[-8px] sm:right-[15px] md:top-[-9px] md:right-[15px] lg:top-[-12px] lg:right-[0px] sm:w-[70px] sm:h-[30px] md:w-[75px] md:h-[35px] lg:w-[100px] lg:h-[40px] m-0'/>
                 </button>
             </form>
         </div>
@@ -67,5 +66,3 @@ const Login: React.FC = () => {
 
   );
 };
-
-export default Login;
