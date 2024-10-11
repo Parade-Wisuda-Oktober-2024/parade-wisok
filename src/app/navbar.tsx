989769985bbd0ba81567e7788179ec8303a2c4dd
wisaahmaduta <wiseowisa@gmail.com>
2024-10-11 23:19:03 +0700

    refactored codes
    -changed sidebar from previously separate fragment into a separate shadcn ui component
    -lower the height of the navigation bar for the desktop size

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,11 +2,16 @@
 import Image from "next/image"
 import logo from "../../public/logo.png"
 import burger from "../../public/burger.svg"
+import nav from "../../public/navbar-bg.png"
 import { usePathname } from "next/navigation"
 import { useEffect } from "react"
 import Link from "next/link"
 import { useState } from "react"
-import Sidebar from "./sidebar"
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "~/components/ui/sheet"
 
 const NavBar = () => {
   const pathname = usePathname()
@@ -15,7 +20,6 @@ const NavBar = () => {
   const [merchandise, setMerchandise] = useState(false)
   const [sponshorship, setSponsorship] = useState(false)
   const [aboutUs, setAboutUs] = useState(false)
-  const [sidebar, setSidebar] = useState(false)
   useEffect(()=> {
     setTaFair(false)
     setGraduates(false)
@@ -30,8 +34,7 @@ const NavBar = () => {
   },[pathname])
   return(
     <div>
-      {sidebar && <Sidebar setSidebar={setSidebar} taFair={taFair} graduates={graduates} merchandise={merchandise} sponshorship={sponshorship} aboutUs={aboutUs} />}
-      <div className="h-[73px] xl:h-[100px] w-full">
+      <div className="h-[73px] xl:h-[83px] w-full">
         <div className="bg-[url('/navbar-bg.png')] w-full h-full bg-center relative bg-cover">
           <div className="bg-[url('/ornament.png')] w-full h-full bg-center bg-cover flex xl:justify-between px-4 xl:px-[40px] bg-opacity-60">
             <div className="flex items-center justify-between gap-2 w-full">
@@ -41,7 +44,36 @@ const NavBar = () => {
                   <div className="h-[28px] font-bold text-3xl text-[#F9BE6B] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] whitespace-nowrap ">WISOKTO 2024</div>
                 </div>
               </Link>
-              <div onClick={()=>setSidebar(true)} className="w-[23.9px] h-[18.59px] xl:hidden"><Image src={burger} alt="dropdown" objectFit="contain"/></div>
+              <Sheet>
+                <SheetTrigger><div className="w-[23.9px] h-[18.59px] xl:hidden"><Image src={burger} alt="dropdown" objectFit="contain"/></div></SheetTrigger>     
+                <SheetContent className="px-[25px] pt-[60px] pb-[30px] text-[28px] text-[#FEE59A] font-header">
+                  <div className="flex flex-col justify-between h-full text-center">
+                    <div className="flex flex-col gap-[16px]">
+                      <Link href="/TaFair">
+                        <div className={`p-[10px] ${!taFair ? 'hover:bg-[#FF8CD926]': ''} rounded-lg transition-all`}>TA Fair<div className={`h-1 bg-[#F9BE6B] rounded-lg transform origin-top transition-all ease-out duration-300 ${taFair ? '-scale-y-100 h-1 opacity-100' : 'scale-y-0 h-0 opacity-0'}`}></div></div>
+                      </Link>
+                      <Link href="/Graduates">
+                        <div className={`p-[10px] ${!graduates ? 'hover:bg-[#FF8CD926]': ''} rounded-lg transition-all`}>Graduates<div className={`h-1 bg-[#F9BE6B] rounded-lg transform origin-top transition-all ease-out duration-300 ${graduates ? '-scale-y-100 h-1 opacity-100' : 'scale-y-0 h-0 opacity-0'}`}></div></div>
+                      </Link>
+                      <Link href="/Merchandise">
+                        <div className={`p-[10px] ${!merchandise ? 'hover:bg-[#FF8CD926]': ''} rounded-lg transition-all`}>Merchandise<div className={`h-1 bg-[#F9BE6B] rounded-lg transform origin-top transition-all ease-out duration-300 ${merchandise ? '-scale-y-100 h-1 opacity-100' : 'scale-y-0 h-0 opacity-0'}`}></div></div>
+                      </Link>
+                      <Link href="/Sponsorship">
+                        <div className={`p-[10px] ${!sponshorship ? 'hover:bg-[#FF8CD926]': ''} rounded-lg transition-all`}>Sponsorship<div className={`h-1 bg-[#F9BE6B] rounded-lg transform origin-top transition-all ease-out duration-300 ${sponshorship ? '-scale-y-100 h-1 opacity-100' : 'scale-y-0 h-0 opacity-0'}`}></div></div>
+                      </Link>
+                      <Link href="/AboutUs">
+                        <div className={`p-[10px] ${!aboutUs ? 'hover:bg-[#FF8CD926]': ''} rounded-lg transition-all`}>About us<div className={`h-1 bg-[#F9BE6B] rounded-lg transform origin-top transition-all ease-out duration-300 ${aboutUs ? '-scale-y-100 h-1 opacity-100' : 'scale-y-0 h-0 opacity-0'}`}></div></div>
+                      </Link>
+                    </div>
+                    <Link href="/Login">
+                      <div className="py-[12px] px-[32px] bg-[#210B3A] rounded-lg border-none font-bold">
+                        Login
+                      </div>
+                    </Link>
+                    <Image src={nav} objectFit="cover" alt="" fill className="-z-10"/>
+                  </div>
+                </SheetContent>
+              </Sheet>
             </div>
             <div className="hidden xl:flex gap-[12px] text-[#F9BE6B] font-bold text-[28px] items-center whitespace-nowrap">
               <Link href="/TaFair">
