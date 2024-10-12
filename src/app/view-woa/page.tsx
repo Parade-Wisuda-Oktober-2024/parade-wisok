60dd81d47dc81ff3e29ba8b486384be77d2dd25e
David Lodianto <david.lodianto@gmail.com>
2024-10-12 22:27:08 +0700

    feat: add show-WOA

---
+++ src/app/view-woa/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WOAList from '~/components/testing/viewWOAExp'
+
+const page = () => {
+  return (
+    <>
+      <WOAList />
+    </>
+  )
+}
+
+export default page
