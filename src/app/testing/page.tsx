a5fd2c22e46e6b0e65148796588b03ec274ff555
andi-frame <andifarhan1094@gmail.com>
2024-10-13 14:51:06 +0700

    feat: add example use for easier integration

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import React from "react";
+import LikeTAExp from "~/components/testing/LikeTAExp";
 import SearchTAExp from "~/components/testing/SearchTAExp";
 
 const Page = () => {
   return (
     <div>
       <SearchTAExp />
+      <LikeTAExp />
     </div>
   );
 };
