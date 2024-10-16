a922733d8d00c65614e95bf3e5aff5c22d5626ed
varelll <19623183@std.stei.itb.ac.id>
2024-10-16 19:55:26 +0700

    add image in sponsorship

--- src/app/sponsorship/page.tsx
+++ src/app/sponsorship/page.tsx
@@ -1,7 +1,28 @@
-import Sponsor1 from "public/logo.png";
-import Sponsor2 from "public/v-1.png";
-import Sponsor3 from "public/v-14.png";
-import Sponsor4 from "public/v-16.png";
+import radio8eh from "public/sponsorship/medpart/8eh-radio-itb.png";
+import aiesec from "public/sponsorship/medpart/aiesec.png";
+import eventdetect from "public/sponsorship/medpart/event-detect.png";
+import eventmahasiswa8 from "public/sponsorship/medpart/event-mahasiswa8.png";
+import ieeeitb from "public/sponsorship/medpart/ieeeitb.jpg";
+import itbstory from "public/sponsorship/medpart/itb-story.png";
+import itbreceh from "public/sponsorship/medpart/itbreceh.png";
+import iyref from "public/sponsorship/medpart/iyref.png";
+import ksep from "public/sponsorship/medpart/ksep.png";
+import lfmitb from "public/sponsorship/medpart/lfm-itb.png";
+import radioraka from "public/sponsorship/medpart/radio-raka.jpg";
+import ruanganevent from "public/sponsorship/medpart/ruangan-event.jpg";
+import sandbox from "public/sponsorship/medpart/sandbox.png";
+import sre from "public/sponsorship/medpart/sre-itb.jpg";
+import tec from "public/sponsorship/medpart/tec-itb.png";
+import temanacara from "public/sponsorship/medpart/temanacara.png";
+import tribunjabar from "public/sponsorship/medpart/tribunjabar.png";
+import urbanradio from "public/sponsorship/medpart/urban-radio.png";
+import eventbanget from "public/sponsorship/medpart/eventbanget.png";
+import swararadio from "public/sponsorship/medpart/swararadio.jpg";
+import bca from "public/sponsorship/sponsor/bca.png";
+import esize from "public/sponsorship/sponsor/esize.png";
+import frestea from "public/sponsorship/sponsor/frestea.png";
+import mahakaX from "public/sponsorship/sponsor/mahakaX.png";
+import propan from "public/sponsorship/sponsor/propan.jpg";
 import SponsorCard from "../../components/sponsorship/SponsorCard";
 import InfiniteCarousel from "../../components/sponsorship/InfiniteCarousel";
 import Title from "~/components/title";
@@ -9,50 +30,109 @@ import BackgroundImage from "public/sponsorship/bg-sponsorship.png";
 
 const Sponsors: React.FC = () => {
   return (
-    
-    <section 
-      className="w-full py-16 flex flex-col items-center" 
+    <section
+      className="flex w-full flex-col items-center py-16"
       style={{
         backgroundImage: `url(${BackgroundImage.src})`,
         backgroundPosition: "center",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
-        }} 
+      }}
     >
       <Title className="mb-5">Sponsorship</Title>
-
-      <div className="grid grid-row-1 sm:grid-row-2 lg:grid-row-3 m-auto mt-10 mb-10 w-full max-w-7xl">
+      <div className="grid-col-1 md:grid-col-2 lg:grid-col-3 m-auto mb-10 mt-10 grid w-full max-w-7xl">
         <div className="flex flex-wrap justify-center">
-          <SponsorCard sponsorImgUrl={Sponsor1} />
-          <SponsorCard sponsorImgUrl={Sponsor2} />
-          <SponsorCard sponsorImgUrl={Sponsor3} />
-          <SponsorCard sponsorImgUrl={Sponsor4} />
-          <SponsorCard sponsorImgUrl={Sponsor1} />
-          <SponsorCard sponsorImgUrl={Sponsor2} />
-          <SponsorCard sponsorImgUrl={Sponsor3} />
-          <SponsorCard sponsorImgUrl={Sponsor4} />
+          <SponsorCard sponsorImgUrl={bca} />
+          <SponsorCard sponsorImgUrl={esize} />
+          <SponsorCard sponsorImgUrl={frestea} />
+          <SponsorCard sponsorImgUrl={mahakaX} />
+          <SponsorCard sponsorImgUrl={propan} />
         </div>
       </div>
       <Title className="mb-5">Media Partner</Title>
       <InfiniteCarousel direction="left">
-        <SponsorCard sponsorImgUrl={Sponsor1} />
-        <SponsorCard sponsorImgUrl={Sponsor2} />
-        <SponsorCard sponsorImgUrl={Sponsor3} />
-        <SponsorCard sponsorImgUrl={Sponsor4} />
-        <SponsorCard sponsorImgUrl={Sponsor1} />
-        <SponsorCard sponsorImgUrl={Sponsor2} />
-        <SponsorCard sponsorImgUrl={Sponsor3} />
-        <SponsorCard sponsorImgUrl={Sponsor4} />
+        <SponsorCard sponsorImgUrl={radio8eh} />
+        <SponsorCard sponsorImgUrl={aiesec} />
+        <SponsorCard sponsorImgUrl={eventdetect} />
+        <SponsorCard sponsorImgUrl={eventmahasiswa8} />
+        <SponsorCard sponsorImgUrl={eventbanget} />
+        <SponsorCard sponsorImgUrl={ieeeitb} />
+        <SponsorCard sponsorImgUrl={itbstory} />
+        <SponsorCard sponsorImgUrl={itbreceh} />
+        <SponsorCard sponsorImgUrl={iyref} />
+        <SponsorCard sponsorImgUrl={ksep} />
+        <SponsorCard sponsorImgUrl={lfmitb} />
+        <SponsorCard sponsorImgUrl={radioraka} />
+        <SponsorCard sponsorImgUrl={swararadio} />
+        <SponsorCard sponsorImgUrl={ruanganevent} />
+        <SponsorCard sponsorImgUrl={sandbox} />
+        <SponsorCard sponsorImgUrl={sre} />
+        <SponsorCard sponsorImgUrl={tec} />
+        <SponsorCard sponsorImgUrl={temanacara} />
+        <SponsorCard sponsorImgUrl={tribunjabar} />
+        <SponsorCard sponsorImgUrl={urbanradio} />
+        <SponsorCard sponsorImgUrl={radio8eh} />
+        <SponsorCard sponsorImgUrl={aiesec} />
+        <SponsorCard sponsorImgUrl={eventdetect} />
+        <SponsorCard sponsorImgUrl={eventmahasiswa8} />
+        <SponsorCard sponsorImgUrl={eventbanget} />
+        <SponsorCard sponsorImgUrl={ieeeitb} />
+        <SponsorCard sponsorImgUrl={itbstory} />
+        <SponsorCard sponsorImgUrl={itbreceh} />
+        <SponsorCard sponsorImgUrl={iyref} />
+        <SponsorCard sponsorImgUrl={ksep} />
+        <SponsorCard sponsorImgUrl={lfmitb} />
+        <SponsorCard sponsorImgUrl={radioraka} />
+        <SponsorCard sponsorImgUrl={swararadio} />
+        <SponsorCard sponsorImgUrl={ruanganevent} />
+        <SponsorCard sponsorImgUrl={sandbox} />
+        <SponsorCard sponsorImgUrl={sre} />
+        <SponsorCard sponsorImgUrl={tec} />
+        <SponsorCard sponsorImgUrl={temanacara} />
+        <SponsorCard sponsorImgUrl={tribunjabar} />
+        <SponsorCard sponsorImgUrl={urbanradio} />
       </InfiniteCarousel>
       <InfiniteCarousel direction="right">
-        <SponsorCard sponsorImgUrl={Sponsor1} />
-        <SponsorCard sponsorImgUrl={Sponsor2} />
-        <SponsorCard sponsorImgUrl={Sponsor3} />
-        <SponsorCard sponsorImgUrl={Sponsor4} />
-        <SponsorCard sponsorImgUrl={Sponsor1} />
-        <SponsorCard sponsorImgUrl={Sponsor2} />
-        <SponsorCard sponsorImgUrl={Sponsor3} />
-        <SponsorCard sponsorImgUrl={Sponsor4} />
+        <SponsorCard sponsorImgUrl={radio8eh} />
+        <SponsorCard sponsorImgUrl={aiesec} />
+        <SponsorCard sponsorImgUrl={eventdetect} />
+        <SponsorCard sponsorImgUrl={eventmahasiswa8} />
+        <SponsorCard sponsorImgUrl={eventbanget} />
+        <SponsorCard sponsorImgUrl={ieeeitb} />
+        <SponsorCard sponsorImgUrl={itbstory} />
+        <SponsorCard sponsorImgUrl={itbreceh} />
+        <SponsorCard sponsorImgUrl={iyref} />
+        <SponsorCard sponsorImgUrl={ksep} />
+        <SponsorCard sponsorImgUrl={lfmitb} />
+        <SponsorCard sponsorImgUrl={radioraka} />
+        <SponsorCard sponsorImgUrl={swararadio} />
+        <SponsorCard sponsorImgUrl={ruanganevent} />
+        <SponsorCard sponsorImgUrl={sandbox} />
+        <SponsorCard sponsorImgUrl={sre} />
+        <SponsorCard sponsorImgUrl={tec} />
+        <SponsorCard sponsorImgUrl={temanacara} />
+        <SponsorCard sponsorImgUrl={tribunjabar} />
+        <SponsorCard sponsorImgUrl={urbanradio} />
+        <SponsorCard sponsorImgUrl={radio8eh} />
+        <SponsorCard sponsorImgUrl={aiesec} />
+        <SponsorCard sponsorImgUrl={eventdetect} />
+        <SponsorCard sponsorImgUrl={eventmahasiswa8} />
+        <SponsorCard sponsorImgUrl={eventbanget} />
+        <SponsorCard sponsorImgUrl={ieeeitb} />
+        <SponsorCard sponsorImgUrl={itbstory} />
+        <SponsorCard sponsorImgUrl={itbreceh} />
+        <SponsorCard sponsorImgUrl={iyref} />
+        <SponsorCard sponsorImgUrl={ksep} />
+        <SponsorCard sponsorImgUrl={lfmitb} />
+        <SponsorCard sponsorImgUrl={radioraka} />
+        <SponsorCard sponsorImgUrl={swararadio} />
+        <SponsorCard sponsorImgUrl={ruanganevent} />
+        <SponsorCard sponsorImgUrl={sandbox} />
+        <SponsorCard sponsorImgUrl={sre} />
+        <SponsorCard sponsorImgUrl={tec} />
+        <SponsorCard sponsorImgUrl={temanacara} />
+        <SponsorCard sponsorImgUrl={tribunjabar} />
+        <SponsorCard sponsorImgUrl={urbanradio} />
       </InfiniteCarousel>
     </section>
   );
--- src/components/sponsorship/SponsorCard.tsx
+++ src/components/sponsorship/SponsorCard.tsx
@@ -10,14 +10,18 @@ const SponsorCard: React.FC<SponsorCardProps> = ({
   altText = "Sponsor Logo",
 }) => {
   return (
-    <div className="sponsor-card p-4 md:p-6 shadow-xl rounded-xl ml-0.5 sm:ml-1 lg:ml-2 mt-1 mb-1">
-      <Image
-        src={sponsorImgUrl} 
-        alt={altText}
-        width={100}
-        objectFit="contain"
-        className="m-auto w-[50px] sm:w-[75px] md:w-[100px]"
-      />
+    <div className="sponsor-card p-4 md:p-6 shadow-xl rounded-xl ml-0.5 sm:ml-1 lg:ml-2 mt-1 mb-1  ">
+      <div className="flex justify-center items-center w-full h-full">
+        <Image
+          src={sponsorImgUrl} 
+          alt={altText}
+          width={100}
+          height={100}
+          objectFit="contain"
+          className="m-auto w-[50px] sm:w-[75px] md:w-[100px]"
+        />
+      </div>
+      
     </div>
   );
 };
