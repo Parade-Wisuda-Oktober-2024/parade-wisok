f0c7c7de19bfffd1d68403944f3ee8856c043b22
soezyxstt <16922204@mahasiswa.itb.ac.id>
2024-10-20 20:46:57 +0700

    fix woa

--- src/app/woa/form-dialog.tsx
+++ src/app/woa/form-dialog.tsx
@@ -10,11 +10,15 @@ import {SearchInput} from "~/components/filter/search";
 
 export default function FormDialog() {
   const graduates = [
-    {name: "Ardito Ramadhan", major: "Teknik Informatika", NIM: "13520123"},
-    {name: "Ardito Ramadhan", major: "Teknik Informatika", NIM: "13520123"},
-    {name: "Ardito Ramadhan", major: "Teknik Informatika", NIM: "13520123"},];
+    {name: "Ardito Ramadhan", major: "Teknik Informatika", NIM: "13520123", id: "hjasy12"},
+    {name: "Ardito Pramono", major: "Teknik Perminyakan", NIM: "13520123", id: "hjasy13"},
+    {name: "Pajarotan Hasibuan", major: "Teknik Mesin", NIM: "13520123", id: "dsjdss"},
+    {name: "Simanungsong Silalahi", major: "Teknik Material", NIM: "13520123", id: "hjasy15"},
+    {name: "Gibran Raditya", major: "Teknik Elektro", NIM: "13520123", id: "hjasy16"},
+    {name: "Baharudin Harahap", major: "Teknik Geologi", NIM: "13520123", id: "hjasy17"},];
   const [isOpen, setIsOpen] = React.useState(false);
   const [isTyping, setIsTyping] = React.useState(false);
+  const [selectedGraduates, setSelectedGraduates] = React.useState<typeof graduates[number] | undefined>(undefined);
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild data-aos="fade-up">
@@ -46,11 +50,18 @@ export default function FormDialog() {
                            onFocus={() => setIsTyping(true)}
                            onBlur={() => setIsTyping(false)}>
                 <div
-                  className={`absolute w-full grid transition-all duration-[2000] min-h-0 overflow-hidden ${isTyping ? "[grid-template-rows:_1fr] top-14 " : "[grid-template-rows:_0fr] top-12"}`}>
+                  className={`absolute w-full grid [transition-property:_height,_top,_border-width] rounded-md [transition-delay:_200ms,_0s,_200s] duration-300 border-[#F4D38E] min-h-0 overflow-hidden ${isTyping ? " top-14 border-2" : "border-0 top-12"}`}
+                  style={{
+                    height: isTyping ? "200px" : "0",
+                  }}>
                   <div
-                    className={`bg-[#421370] h-max min-h-0 w-full rounded-md transition-all duration-[2000] border-[#F4D38E] ${isTyping && "first:mt-2 border-2 last:mb-2"} px-4 divide-amber-300 divide-y-2 `}>
-                    {graduates.map(({name, major, NIM}) => (
-                      <div className="flex py-2 w-full text-[#F4D38E] justify-between" key={name + major}>
+                    className={`bg-[#421370] overflow-y-scroll min-h-0 w-full rounded-md ${isTyping && "first:mt-2 last:mb-2"} px-4 divide-amber-300 divide-y-2 `}>
+                    {graduates.map(({name, major, NIM, id}, i) => (
+                      <div className="flex py-2 w-full text-[#F4D38E] justify-between" key={i + id}
+                           onClick={() => {
+                             setSelectedGraduates({name, major, NIM, id});
+                             setIsTyping(false);
+                           }}>
                         <div className="">
                           <h4 className='capitalize'>{name}</h4>
                           <h4 className='capitalize'>{NIM}</h4>
@@ -61,6 +72,9 @@ export default function FormDialog() {
                   </div>
                 </div>
               </SearchInput>
+              {!!selectedGraduates && (
+                <h4 className="text-base md:text-xl italic mt-4 underline">{`To: ${selectedGraduates.name} - ${selectedGraduates.major}`}</h4>
+              )}
               <MenfessForm
                 targetUserId={"hjasy12"}
                 onFinishSubmit={() => setIsOpen(false)}
