81afa6c64a77dc2daaf453dd30dc970177247354
Adi Haditya Nursyam <139353811+soezyxstt@users.noreply.github.com>
2024-10-23 16:51:58 +0700

    Update FAQitem.tsx

--- src/components/FAQ/FAQitem.tsx
+++ src/components/FAQ/FAQitem.tsx
@@ -14,10 +14,10 @@ interface FAQItemProps {
 
 const FAQItem: React.FC<FAQItemProps> = ({ question, answer, value }) => {
     return (
-        <div className="md:w-[70%] w-[80%] flex flex-col items-center justify-center rounded-xl border-[3px] border-[#9803f591] bg-gradient-to-b from-[#10092b4a] to-[#03010823] mb-4">            
+        <div className="md:w-[70%] w-[90%] flex flex-col items-center justify-center rounded-xl border-[3px] border-[#9803f591] bg-gradient-to-b from-[#10092b4a] to-[#03010823] mb-4">            
                 <AccordionItem className="w-full" value={value}>
-                    <AccordionTrigger className="px-5 text-2xl font-[Fraunces] text-[#D9D9D9] font-bold">{question}</AccordionTrigger>
-                        <AccordionContent className="px-5 font-[Fraunces] text-[#D9D9D9]">
+                    <AccordionTrigger className="px-5 md:text-xl lg:text-2xl text:base font-[Fraunces] text-[#D9D9D9] font-bold">{question}</AccordionTrigger>
+                        <AccordionContent className="px-5 font-[Fraunces] md:text-lg text-[#D9D9D9]">
                         {answer}
                         </AccordionContent>
                 </AccordionItem>
